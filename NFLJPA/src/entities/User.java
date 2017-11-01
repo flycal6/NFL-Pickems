be6704c62e6d7eaa662b53c2a91dae030d167ec4
Jimmy Easter <jmeaster1@gmail.com>
2017-11-01 10:47:38 -0600

    add Week, Pick, User entities

---
+++ NFLJPA/src/entities/User.java
@@ -0,0 +1,141 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String fName;
+	
+	private String lName;
+	
+	private String email;
+	
+	private String password;
+	
+	private double points;
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public double getPoints() {
+		return points;
+	}
+
+	public void setPoints(double points) {
+		this.points = points;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public User(int id, String fName, String lName, String email, String password, double points) {
+		super();
+		this.id = id;
+		this.fName = fName;
+		this.lName = lName;
+		this.email = email;
+		this.password = password;
+		this.points = points;
+	}
+
+	public User() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", fName=" + fName + ", lName=" + lName + ", email=" + email + ", password="
+				+ password + ", points=" + points + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((fName == null) ? 0 : fName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lName == null) ? 0 : lName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(points);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (fName == null) {
+			if (other.fName != null)
+				return false;
+		} else if (!fName.equals(other.fName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lName == null) {
+			if (other.lName != null)
+				return false;
+		} else if (!lName.equals(other.lName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (Double.doubleToLongBits(points) != Double.doubleToLongBits(other.points))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ NFLJPA/src/entities/Week.java
@@ -0,0 +1,82 @@
+package entities;
+
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+public class Week {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+	public Week(int id, Date date) {
+		super();
+		this.id = id;
+		this.date = date;
+	}
+
+	public Week() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Week [id=" + id + ", date=" + date + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Week other = (Week) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+    
+    
+    
+    
+}
