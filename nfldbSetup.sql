1a6fc19e662a0b2b35215fb9d8a222980e73363c
Brian Thomas <brianraythomas@gmail.com>
2017-11-01 13:57:54 -0600

    create user changes

--- nfldbSetup.sql
+++ nfldbSetup.sql
@@ -4,10 +4,10 @@ CREATE DATABASE nfldb;
 USE nfldb;
 
 -- Add user
--- Uncomment the line below if you have already created the user
+-- Uncomment the lines below as needed
 -- DROP USER 'nfl'@'localhost';
-CREATE USER 'nfl'@'localhost' IDENTIFIED BY 'nfl';
-GRANT ALL PRIVILEGES ON nfldb.* TO 'nfl'@'localhost';
+-- CREATE USER 'nfl'@'localhost' IDENTIFIED BY 'nfl';
+-- GRANT ALL PRIVILEGES ON nfldb.* TO 'nfl'@'localhost';
 
 --  Build tables
 DROP TABLE IF EXISTS `user`;
