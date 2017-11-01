c5716105ccb906b8843bf3703cdbc51cc2787243
dougkucera <dkj1715@gmail.com>
2017-11-01 16:24:07 -0600

    User DAO updated

--- NFLREST/src/data/UserDAO.java
+++ NFLREST/src/data/UserDAO.java
@@ -1,5 +1,21 @@
 package data;
 
-public interface UserDAO {
+import java.util.List;
 
+import entities.League;
+import entities.User;
+
+public interface UserDAO {
+	
+	public User showUser(int uid);
+	
+	public User updateUser(int uid, String todoJson);
+	
+	public Boolean destroyUser(int uid);
+	
+	public League joinLeague(User u);
+	
+	public List<League> leagueIndex(); //Show available leagues
+	
+	
 }
