d581fdacf398c44e3d254d508a535cf9e2d132b5
Jimmy Easter <jmeaster1@gmail.com>
2017-11-01 08:42:24 -0600

    stub out ping test

--- NFLREST/src/controllers/NFLController.java
+++ NFLREST/src/controllers/NFLController.java
@@ -1,5 +1,26 @@
 package controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import data.NFLDAO;
+
+@RestController
 public class NFLController {
+	
+	@Autowired
+	NFLDAO dao;
+	
+	@RequestMapping(path="ping", method=RequestMethod.GET)
+	public String ping(){
+	  return "pong";
+	}
+	
 
 }
