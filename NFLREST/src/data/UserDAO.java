1f507bb10c284c7fd61f2c37d4ca9d313999312f
Jimmy Easter <jmeaster1@gmail.com>
2017-11-02 09:33:36 -0600

    add UserDAOImpl

--- NFLREST/src/data/UserDAO.java
+++ NFLREST/src/data/UserDAO.java
@@ -7,15 +7,13 @@ import entities.User;
 
 public interface UserDAO {
 	
+	public List<User> indexUser(int lid);
+	
 	public User showUser(int uid);
 	
 	public User updateUser(int uid, String todoJson);
 	
 	public Boolean destroyUser(int uid);
-	
-	public League joinLeague(User u);
-	
-	public List<League> leagueIndex(); //Show available leagues
-	
+	 
 	
 }
--- NFLREST/src/data/UserDAOImpl.java
+++ NFLREST/src/data/UserDAOImpl.java
@@ -1,5 +1,71 @@
 package data;
 
-public class UserDAOImpl {
+import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import entities.League;
+import entities.User;
+
+@Transactional
+public class UserDAOImpl implements UserDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public List<User> indexUser(int lid) {
+		String query = "SELECT u FROM User u WHERE u.league.id = :lid ";
+		List<User> users = em.createQuery(query, User.class).setParameter("lid", lid).getResultList();
+		return users;
+	}
+	
+	@Override
+	public User showUser(int uid) {
+		String query = "SELECT u FROM User u WHERE u.id = :uid";
+		User user = em.createQuery(query, User.class).setParameter("uid", uid).getSingleResult();
+		return user;
+	}
+
+	@Override
+	public User updateUser(int uid, String todoJson) {
+//		User user = em.find(User.class, uid);
+		User managedUser = em.find(User.class, uid);
+		User user = null;
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			  user = mapper.readValue(todoJson, User.class);
+			} catch (Exception e) {
+			  e.printStackTrace();
+			}
+		
+		managedUser.setfName(user.getfName());
+		managedUser.setlName(user.getlName());
+		managedUser.setEmail(user.getEmail());
+		managedUser.setPassword(user.getPassword());
+		managedUser.setPoints(user.getPoints());
+		managedUser.setPicks(user.getPicks());
+		managedUser.setLeagues(user.getLeagues());
+		
+		return managedUser;
+	}
+
+	@Override
+	public Boolean destroyUser(int uid) {
+		User user = em.find(User.class, uid);
+		try {
+			em.remove(user);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
