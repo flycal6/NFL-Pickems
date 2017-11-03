8f4001aa5ea0afb923b41c4697059db3d4914ef1
dougkucera <dkj1715@gmail.com>
2017-11-03 14:56:44 -0600

    Game changes

--- NFLREST/WebContent/app/appModule/game.service.js
+++ NFLREST/WebContent/app/appModule/game.service.js
@@ -1,5 +1,6 @@
 angular.module('appModule').factory('gameService', function($http, $filter, $location, authService){
 	var service = {};
+	var uid = authService.getToken().id;
 	
 	
 	service.indexGame = function(wid){
@@ -9,6 +10,32 @@ angular.module('appModule').factory('gameService', function($http, $filter, $loc
 		});
 	};
 	
+//Sends array of picks to Pick table	
+	service.createPicks = function(picksArr){
+		
+		return $http({
+			method: 'POST',
+			url: 'rest/users/' + user.id + '/leagues/' + id
+		});
+	};
+//SERVICE TO ADD NEW ITEM TO LIST	  
+  service.createPicks = function(picksArr){
+	  
+	  return $http({
+	  		method: "POST",
+			url:  "rest/user/" +uid+ "/picks",
+			headers: {
+	  			'Content-Type' : 'application/json'
+	  		},
+	  		data : picksArr
+	  	})
+	  	.then(function(res){
+		
+
+			return res;
+		})
+	
+	};	
 
 	
 	return service;
