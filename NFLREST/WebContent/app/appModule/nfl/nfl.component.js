c8f61c40b311ce3c91e1bbf2843634f8ef3d4f41
Jimmy Easter <jmeaster1@gmail.com>
2017-11-07 10:02:57 -0700

    Add create league form

--- NFLREST/WebContent/app/appModule/nfl/nfl.component.js
+++ NFLREST/WebContent/app/appModule/nfl/nfl.component.js
@@ -1,7 +1,15 @@
 angular.module('appModule').component('nfl', {
-	templateUrl: 'app/appModule/nfl/nfl.component.html',
-	controller: function(nflService){
+	templateUrl : 'app/appModule/nfl/nfl.component.html',
+	controller : function(nflService) {
 		var vm = this;
+		vm.newLeague = null;
+
+		vm.showNewLeagueForm = function() {
+			vm.newLeague = 1;
+		}
+		vm.cancelNewLeagueForm = function() {
+			vm.newLeague = null;
+		}
 	},
-	controllerAs: 'vm'
+	controllerAs : 'vm'
 });
