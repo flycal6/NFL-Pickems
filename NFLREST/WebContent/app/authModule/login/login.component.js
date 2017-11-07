e8bc17a44f9db02499390a047eae5b9a012782a3
Jimmy Easter <jmeaster1@gmail.com>
2017-11-07 13:06:13 -0700

    login loading

--- NFLREST/WebContent/app/authModule/login/login.component.js
+++ NFLREST/WebContent/app/authModule/login/login.component.js
@@ -2,10 +2,13 @@ angular.module('authModule').component('login', {
 	templateUrl: 'app/authModule/login/login.component.html',
 	controller: function(authService, $location){
 		var vm = this;
+		vm.loading = 0;
 		
 		vm.login = function(user){
+		   	vm.loading = 1;
 			authService.login(user).then(function(res){
 				$location.path('/');
+				vm.loading = 0;
 			});
 		}
 	},
